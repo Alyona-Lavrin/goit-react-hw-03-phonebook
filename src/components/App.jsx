e1386f4c5377c76ddaf9451abab529e274a232f5
Alona <polejcukalona@gmail.com>
2023-10-14 17:17:44 +0300

    sdsgg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,100 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList'
+
+export class App extends Component {
+
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  async componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+
+  addCantact = (value) => {
+    const { contacts } = this.state;
+
+    if (contacts.filter(({name}) => name.toLowerCase() === value.name.toLowerCase()).length > 0) {
+      alert(`${value.name} is already in contacts`);
+      return;
+    }
+
+    const data = {
+      id: nanoid(),
+      name: value.name,
+      number: value.number
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, data],
+    }));
+  }
+
+  getVisibleItems = () => {
+    const { contacts, filter } = this.state;
+
+    let result = contacts
+    
+    if(filter !== '') {
+      result = contacts.filter(({name}) => name.toLowerCase().includes(filter.toLowerCase()))
+    }
+    
+    return result;
+  };
+
+  deleteContact = (deleteId) => {
+    const { contacts } = this.state;
+
+    const result = contacts.filter(({id}) => id !== deleteId)
+    this.setState(() => ({
+      contacts: result,
+    }));
+  }
+
+  setFilter = (string) => {
+    this.setState(() => ({
+      filter: string,
+    }));
+  }
+  
+
+  render() {
+    const visibleItems = this.getVisibleItems();
+    
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onAddContact={this.addCantact} />
+        <h2>Contacts</h2>
+        <Filter onFilterContact={this.setFilter}/>
+        <ContactList contacts={visibleItems} onDeleteContact={this.deleteContact}/>
+      </div>
+    );
+  };
+}
---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const ContactsUl = styled.ul`
+  width: 400px;
+  padding: 20px;
+  padding-left: 40px;
+  box-sizing: border-box;
+  margin: 0;
+  list-style: disc;
+`;
+
+export const Contactsli = styled.li`
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
+
+export const DelBtn = styled.button`
+  width: auto;
+  height: 30px;
+  background: white;
+  border-radius: 6px;
+`;
